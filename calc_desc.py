efb0333e51384f57219625451cf7d846899b79f0
ubb3rsith <aesouza.jvm@gmail.com>
2013-09-21 13:30:12 -0300

    Calcula o valor final de uma mercadoria baseado em preço - % desconto.

---
+++ calc_desc.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#_*_ encoding: utf-8 _*_
+#Lista 1 de exercícios
+#5) Solicite o preço de uma mercadoria e o percentual de desconto. Exiba o valor do desconto e o
+#   preço a pagar.
+def calc(preco,perc):
+    desconto = preco*perc/100
+    print("Desconto: %d" %desconto)
+    print("Total a pagar: %d" %(preco - desconto))
+
+#para simplificar, trabalhando apenas com valores/numeros inteiros :]
+preco = int (input("Preço: "))
+perc = int(input("Percentual de desconto (%): "))
+calc(preco,perc)
+
+
