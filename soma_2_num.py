a1003e368c7375081fc05b4d237b51a139f3b7a2
ubb3rs17h <aesouza.jvm@gmail.com>
2013-09-21 10:15:01 -0300

    Exercicio 1 da primeira lista do curso

---
+++ soma_2_num.py
@@ -0,0 +1,6 @@
+#Exercicio Python4Zombies
+#Padrão para nome: exercicioI_nomealuno_numero_questao.py
+#Questão: 1) Faça um programa que peça dois números inteiros e imprima a soma desses dois números
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o segundo: "))
+print("Soma: ",n1+n2)
