0553e6176d2c8091b03364684486be0464dd5d86
ubersith <aesouza.jvm@gmail.com>
2013-09-23 14:45:31 -0300

    Calculo de gasto total

--- preco_total_car.py
+++ preco_total_car.py
@@ -5,10 +5,10 @@
 #   usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar,
 #   sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por km rodado.
 
-def calc(dias,km):
+def calc(dias,kmRod):
     return float ((dias * 60.00)+(km * 0.15))
 
-km =  float(input ("Quantidade de kilometros rodado: "))
+kmRod =  float(input ("Quantidade de kilometros rodado: "))
 dias = int (input ("Total de dias alugados: "))
-print ("Valor total: R$%.2f" %calc(dias,km))
+print ("Valor total [R$]: %.2f" %calc(dias,kmRod))
 
