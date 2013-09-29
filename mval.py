4e558e213f7f81b2cc82b54170a9f5919354e6e5
ubb3rsith <aesouza.jvm@gmail.com>
2013-09-29 14:03:42 -0300

    Retorna o maior valor entre 3 numeros

---
+++ mval.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#_*_ encoding: utf-8 _*_
+#Lista 2 de exercicios
+#Faça um Programa que leia três números e mostre o maior deles.
+
+def maior(n1,n2,n3):
+    return max(n1,n2,n3)
+
+#teste
+print (maior(30,20,30))
+    
+
+
