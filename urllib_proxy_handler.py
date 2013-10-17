91b1ce06cd7f94e9f574a5b2a98b4b934548d15f
ubb3rsith <aesouza.jvm@gmail.com>
2013-10-17 08:05:24 -0300

    Port para python3, try-catch para uso ou nao de autenticao e json para recuperar conteudo

--- urllib_proxy_handler.py
+++ urllib_proxy_handler.py
@@ -4,29 +4,44 @@
 # Date:   27/10/2008
 #
 # Example for using urllib2.urlopen() with a proxy server requiring authentication
-import urllib2
-uri = "http://www.python.org"
-http_proxy_server = "someproxyserver.com"
-http_proxy_port = "3128"
+'''
+17/10/2013 - ubb3rsith
+Conversion of python2 to python3
+First, try connect without proxy...if raise some Exception, call proxy authentication function
+Json Killer mode on ;]
+'''
+import urllib.request #substitution for the urllib2 lib of python2
+import json
+
+uri = "http://api.icndb.com/jokes/random?limitTo=[nerdy]" #Chuck Norris jokes... ;]
+http_proxy_server = "someproxyserver"
+http_proxy_port = "port"
 http_proxy_realm = http_proxy_server # Worked in my (limited) testing environment.
-http_proxy_user = "username"
-http_proxy_passwd = "password"
+http_proxy_user = "user"
+http_proxy_passwd = "passwd"
 # Next line = "http://username:password@someproxyserver.com:3128"
 http_proxy_full_auth_string = "http://%s:%s@%s:%s" % (http_proxy_user,
                             http_proxy_passwd,
                             http_proxy_server,
                             http_proxy_port)
 def open_url_no_proxy():
-  urllib2.urlopen(uri)
-  print "Apparent success without proxy server!"
+  try:
+    urllib.request.urlopen(uri)
+    print ("Apparent success without proxy server!")
+  except Exception:
+    #try open url with proxy authentication
+    open_url_installed_opener()
+  finally:
+    pass
+  
 def open_url_installed_opener():
-  proxy_handler = urllib2.ProxyHandler({"http": http_proxy_full_auth_string})
-  opener = urllib2.build_opener(proxy_handler)
-  urllib2.install_opener(opener)
-  urllib2.urlopen(uri)
-  print "Apparent success through proxy server!"
+  proxy_handler = urllib.request.ProxyHandler({"http": http_proxy_full_auth_string})
+  opener = urllib.request.build_opener(proxy_handler)
+  urllib.request.install_opener(opener)
+  resp = urllib.request.urlopen(uri).read()
+  data = data = json.loads(resp.decode('utf-8'))
+  print(data['value']['joke'])
+  print ("Apparent success through proxy server!")
+
 if __name__ == "__main__":
   open_url_no_proxy()
-  open_url_installed_opener()
-
-
